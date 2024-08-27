e0d600cc028ee5e9edc9aab541dfd320090924dd
Aermoss <86446300+Aermoss@users.noreply.github.com>
2024-08-27 06:48:28 +0300

    Bump version to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "UTF-8") as file:
  
 setup(
     name = "forges",
-    version = "0.0.7",
+    version = "0.0.8",
     description = "A game engine made with SDL 2.",
     long_description = long_desc,
     long_description_content_type = "text/markdown",
